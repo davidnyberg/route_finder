bea69f4dce142b1b217b264b447358b2dfbaf1b7
davidnyberg <dnyberg95@gmail.com>
2021-07-13 21:53:44 +0200

    init commmit

---
+++ src/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "pk.eyJ1IjoiZGF2aWRueWJlcmciLCJhIjoiY2tyMmc1cTN5MGswejJvcng1d2owZnR0YyJ9.931rNR_4eqyfhufFGPRMjg";;
+
+const key = {};
+
+export { mapbox, key };
